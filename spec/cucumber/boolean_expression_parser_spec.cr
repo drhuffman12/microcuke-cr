03698ff2470b3b446ba508a1ab50f4f516a6787b
Daniel Huffman <drhuffman12@yahoo.com>
2017-04-07 08:06:41 -0400

    comment cleanup

--- spec/cucumber/boolean_expression_parser_spec.cr
+++ spec/cucumber/boolean_expression_parser_spec.cr
@@ -88,45 +88,4 @@ Spec2.describe Cucumber::BooleanExpressionParser do
       expect(expr.to_s).to eq(outfix)
     end
   end
-
-  # describe "#parse" do
-  #   [
-  #     ["a and b", "( a and b )"],
-  #     # ["a or b", "( a or b )"],
-  #     # ["not a", "not ( a )"],
-  #     # ["( a and b ) or ( c and d )", "( ( a and b ) or ( c and d ) )"],
-  #     #@ ["( a and b ) or ( c and d )", "( ( ( a and b ) ) or ( ( c and d ) ) )"],
-  #     # ["not a or b and not c or not d or e and f", "( ( ( not ( a ) or ( b and not ( c ) ) ) or not ( d ) ) or ( e and f ) )"]
-  #     # a or not b
-  #   ].each do |in_out|
-  #     # puts "infix: #{infix}"
-  #     # puts "outfix: #{outfix}"
-  #     puts "in_out: #{in_out}"
-  #     @in_out = in_out
-  #     # @infix = in_out[0]
-  #     # @outfix = in_out[1]
-  #     # let(infix) { @in_out[0] }
-  #     # let(outfix) { @in_out[1] }
-  #     it "a and b" do
-  #       # infix = "a and b"
-  #       # outfix = "( a and b )"
-  #       infix = @in_out[0]
-  #       outfix = @in_out[1]
-  #       expr = parser.parse(infix)
-
-  #       puts "infix: #{infix}"
-  #       infix_str = "#{infix}"
-  #       outfix_str = "#{outfix}"
-  #       puts "outfix: #{outfix}"
-  #       puts "expr: #{expr || "(nil)"}"
-
-  #       expect(expr.to_s).to eq(outfix)
-  #     end
-  #   end
-
-  #   # it "TODO" do
-  #   #   expect(false).to eq(true)
-  #   # end
-  # end
-
 end
